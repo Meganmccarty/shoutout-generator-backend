be1645e5cdb7fb03c63c3b1636f095251ef3c250
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-22 12:29:56 -0400

    Create quote model and db table

---
+++ db/migrate/20211022162134_create_quotes.rb
@@ -0,0 +1,10 @@
+class CreateQuotes < ActiveRecord::Migration[6.1]
+    def change
+        create_table :quotes do |t|
+            t.string :quote
+            t.string :author
+
+            t.timestamps
+        end
+    end
+end
