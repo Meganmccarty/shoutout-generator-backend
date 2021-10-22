d3f2270b113b9ee76e46c9cc4ed885115f537296
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-22 13:10:46 -0400

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,102 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quotes = Quote.create([
+    {
+        text: "Positive anything is better than negative nothing.", 
+        author: "Elbert Hubbard"
+    },
+    {
+        text: "Miracles happen to those who believe in them.",
+        author: "Bernhard Berenson"
+    },
+    {
+        text: "One small positive thought can change your whole day.",
+        author: "Zig Ziglar"
+    },
+    {
+        text: "You can do this!"
+    },
+    {
+        text: "You've got this!"
+    },
+    {
+        text: "Just believe in yourself!"
+    },
+    {
+        text: "Believe you can and you're halfway there.",
+        author: "Teddy Roosevelt"
+    },
+    {
+        text: "Be positive. Be true. Be kind.",
+        author: "Roy T. Bennett"
+    },
+    {
+        text: "If you are positive, you'll see opportunities instead of obstacles.",
+        author: "Widad Akrawi"
+    },
+    {
+        text: "Write it on your heart that every day is the best day in the year.",
+        author: "Ralph Waldo Emerson"
+    },
+    {
+        text: "Accentuate the positive, Eliminate the Negative, latch onto the affirmative.",
+        author: "Bing Crosby"
+    },
+    {
+        text: "The best is yet to be.",
+        author: "Robert Browning"
+    },
+    {
+        text: "Try to be a rainbow in someone’s cloud.",
+        author: "Maya Angelou"
+    },
+    {
+        text: "Turn every life situation into a positive one.",
+        author: "Rhonda Byrne"
+    },
+    {
+        text: "Believe that life is worth living and your belief will help create the fact.",
+        author: "William James"
+    },
+    {
+        text: "Do good and good will come to you."
+    },
+    {
+        text: "Surround yourself with positive people and you’ll be a positive person.",
+        author: "Kellie Pickler"
+    },
+    {
+        text: "A positive mindset brings positive things.",
+        author: "Phillip Reiter"
+    },
+    {
+        text: "When things go wrong, don’t go with them.",
+        author: "Elvis Presley"
+    },
+    {
+        text: "When life gets you down, you know what you gotta do? Just keep swimming.",
+        author: "Dory"
+    },
+    {
+        text: "You are so much stronger than you think you are!"
+    },
+    {
+        text: "Tough times don't last. Tough people do",
+        author: "Gregory Peck"
+    },
+    {
+        text: "Be the reason someone smiles today!"
+    },
+    {
+        text: "Don't let anyone dull your sparkle!"
+    },
+    {
+        text: "The best view comes after the hardest climb."
+    },
+    {
+        text: "Nothing is impossible. The word itself says 'I'm possible!",
+        author: "Audrey Hepburn"
+    }
+])
