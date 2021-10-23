7ed6c65b7a0b4a6fde72fa5a35be7490a2c99d64
Megan McCarty <megan_mccarty@hotmail.com>
2021-10-23 09:21:24 -0400

    Configure cors for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+        allow do
+            origins 'http://localhost:3000'
+            resource '*',
+                headers: :any,
+                expose: ["Authorization"],
+                methods: %i[get post put patch delete options head]
+        end
+    end
 end
